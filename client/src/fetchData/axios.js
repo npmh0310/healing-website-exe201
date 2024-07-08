1244d9cce36d9aa97192eb846b20d1cc622ef1ef
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-07-08 10:49:36 +0700

    fix deploy

--- client/src/fetchData/axios.js
+++ client/src/fetchData/axios.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api/v1',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "https://healing-website-exe201.onrender.com/api/v1",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-instance.interceptors.response.use(function (response) {
-    return response
-}, function (error) {
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
     return error.response;
-});
+  }
+);
 
 export default instance;
